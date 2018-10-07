a43b15a4333818e5a15a91ce232eb796a3a85c00
Demo User <debian@beaglebone.localdomain>
2018-10-07 17:44:55 +0000

    Updated python script

--- beagleboneHardware/beagleBoneHardware.py
+++ beagleboneHardware/beagleBoneHardware.py
@@ -1,4 +1,15 @@
+#Config BB pins
+#config-pin P8.8 gpio
+#config-pin P8.10 gpio
+
 import paho.mqtt.client as mqtt
+import Adafruit_BBIO.GPIO as GPIO
+
+#config-pin P8_8 gpio
+#config-pin P8_10 gpio
+
+ledPin = "P8_8"
+pirPin = "P8_10"
 
 # The callback for when the client receives a CONNACK response from the server.
 def on_connect(client, userdata, flags, rc):
@@ -12,11 +23,17 @@ def on_connect(client, userdata, flags, rc):
 def on_message(client, userdata, msg):
    	print(msg.topic+" "+str(msg.payload))
 
+# Conenct to Broker, set up callbacks
 client = mqtt.Client()
 client.on_connect = on_connect
 #client.on_message = on_message
-
 client.connect("192.168.1.34", 1986)
+
+#Set up pins
+GPIO.setup(ledPin, GPIO.OUT)
+GPIO.setup(pirPin, GPIO.IN)
+
+GPIO.output(ledPin, GPIO.HIGH)
 
 # Blocking call that processes network traffic, dispatches callbacks and
 # handles reconnecting.
@@ -25,5 +42,9 @@ client.connect("192.168.1.34", 1986)
 #client.loop_forever(timeout=1.0)
 
 while True:
-	client.publish("presence", "alarmBBB", 2)
+	#GPIO.output(ledPin, GPIO.HIGH)
+	client.publish("presence", "alarm", 2)
 	client.loop()
+	#time.sleep(1)
+	#GPIO.output(ledPin, GPIO.LOw)
+	#time.sleep(1)
