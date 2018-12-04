f7f96e28ccd19649c68ee9127abe4b35bca1f406
Juan Luis Perez Diez <jlpdiez@ucm.es>
2018-12-04 17:12:43 +0100

    Presence sensor attached to scene simulation

--- phatHardwareLink/src/main/java/phat/PhatHardwareLink.java
+++ phatHardwareLink/src/main/java/phat/PhatHardwareLink.java
@@ -1,16 +1,13 @@
 package phat;
 
-import java.rmi.AlreadyBoundException;
-import java.rmi.NotBoundException;
-import java.rmi.RemoteException;
 import java.util.logging.Logger;
 import java.util.logging.Level;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 
-import com.jme3.math.Quaternion;
-import com.jme3.math.Vector3f;
+import com.jme3.app.state.AppStateManager;
 
+import com.jme3.scene.Node;
 import phat.agents.Agent;
 import phat.agents.HumanAgent;
 import phat.agents.automaton.Automaton;
@@ -29,18 +26,22 @@ import phat.agents.automaton.conditions.TimerFinishedCondition;
 import phat.agents.automaton.uses.UseDoorbellAutomaton;
 import phat.agents.commands.ActivateActuatorEventsLauncherCommand;
 import phat.agents.commands.ActivateCallStateEventsLauncherCommand;
-import phat.app.PHATApplication;
 import phat.body.BodiesAppState;
 import phat.body.commands.*;
+import phat.commands.PHATCommand;
+import phat.commands.PHATCommandListener;
 import phat.config.AgentConfigurator;
 import phat.config.BodyConfigurator;
 import phat.config.DeviceConfigurator;
 import phat.config.HouseConfigurator;
 import phat.config.ServerConfigurator;
 import phat.config.WorldConfigurator;
-import phat.gui.logging.PrettyLogViewerAppState;
+import phat.devices.commands.CreatePresenceSensorCommand;
+import phat.sensors.presence.PHATPresenceSensor;
+import phat.sensors.presence.PresenceStatePanel;
+import phat.server.ServerAppState;
+import phat.server.commands.CreateAllPresenceSensorServersCommand;
 import phat.structures.houses.HouseFactory;
-import phat.world.MonitorEventQueueImp;
 import phat.world.WorldAppState;
 
 import org.eclipse.paho.client.mqttv3.MqttClient;
@@ -54,8 +55,8 @@ import java.nio.charset.StandardCharsets;
  * Phat Hardware Link
  * @author melkoroth
  */
-public class PhatHardwareLink implements PHATInitializer {
-    static MonitorEventQueueImp meq = new MonitorEventQueueImp();
+public class PhatHardwareLink implements PHATInitializer, PHATCommandListener {
+    static GUIPHATInterface phat;
 
     String bodyId = "Patient";
     String houseId = "House1";
@@ -67,47 +68,19 @@ public class PhatHardwareLink implements PHATInitializer {
     String mqttClientId = "PhatHardwareLink";
     MemoryPersistence mqttPersistence = new MemoryPersistence();
 
-    public static void main(String[] args) throws RemoteException, AlreadyBoundException, NotBoundException {
+    public static void main(String[] args) {
         //String[] a = {"-record"};
         PhatHardwareLink sim = new PhatHardwareLink();
-        GUIPHATInterface phat = new GUIPHATInterface(sim);//, new GUIArgumentProcessor());
+        phat = new GUIPHATInterface(sim);//, new GUIArgumentProcessor());
         phat.setStatView(true);
         phat.setDisplayFPS(true);
         phat.setSeed(0);
         phat.setDisplayHeight(800);
         phat.setDisplayWidth(480);
-        //phat.setPrettyLogView(false);
-        //phat.app.getStateManager().detach(new PrettyLogViewerAppState());
-
         phat.setTittle("PhatHardwareLink");
         phat.start();
-        phat.setPrettyLogView(false);
-    }
-
-    //TODO: Separar init de mensaje en si
-    private void sendMQTTmessage(String msg) {
-        try {
-            MqttClient sampleClient = new MqttClient(mqttBroker, mqttClientId, mqttPersistence);
-            MqttConnectOptions connOpts = new MqttConnectOptions();
-            connOpts.setCleanSession(true);
-            System.out.println("Connecting to broker: "+mqttBroker);
-            sampleClient.connect(connOpts);
-            System.out.println("Connected");
-            System.out.println("Publishing message: "+msg);
-            MqttMessage message = new MqttMessage(msg.getBytes(StandardCharsets.UTF_8));
-            message.setQos(mqttQos);
-            sampleClient.publish(mqttTopic, message);
-            System.out.println("Message published");
-            sampleClient.disconnect();
-            System.out.println("Disconnected");
-        } catch(MqttException me) {
-            System.out.println("reason "+me.getReasonCode());
-            System.out.println("msg "+me.getMessage());
-            System.out.println("loc "+me.getLocalizedMessage());
-            System.out.println("cause "+me.getCause());
-            System.out.println("excep "+me);
-            me.printStackTrace();
-        }
+        //Hide prettyLogger from GUI
+        phat.hidePrettyLogger();
     }
 
     @Override
@@ -131,7 +104,26 @@ public class PhatHardwareLink implements PHATInitializer {
     }
 
     @Override
-    public void initDevices(DeviceConfigurator deviceConfig) {}
+    public void initDevices(DeviceConfigurator deviceConfig) {
+        AppStateManager stateManager = phat.app.getStateManager();
+        stateManager.attach(phat.deviceConfig.getDevicesAppState());
+        //Create presence sensor
+        CreatePresenceSensorCommand cpsc = new CreatePresenceSensorCommand("PreSen-Bedroom1-1", this);
+        cpsc.setEnableDebug(true);
+        cpsc.sethAngle(90f);
+        cpsc.setvAngle(45f);
+        cpsc.setAngleStep(10f);
+        phat.deviceConfig.getDevicesAppState().runCommand(cpsc);
+        //Create GUI sensor monitor
+        sensorMonitor = new JFrame("Sensor Monitoring");
+        JPanel content = new JPanel();
+        sensorMonitor.setContentPane(content);
+        sensorMonitor.setVisible(true);
+        //Attach sensor to scene
+        ServerAppState serverAppState = new ServerAppState();
+        stateManager.attach(serverAppState);
+        serverAppState.runCommand(new CreateAllPresenceSensorServersCommand());
+    }
 
     @Override
     public void initServer(ServerConfigurator deviceConfig) {}
@@ -231,5 +223,50 @@ public class PhatHardwareLink implements PHATInitializer {
     @Override
     public String getDescription() {
         return "This is a proof of concept simulation with a presence sensor attached which sends MQTT messages out";
+    }
+
+    //Updates GUI for presence sensor
+    @Override
+    public void commandStateChanged(PHATCommand command) {
+        if (command instanceof CreatePresenceSensorCommand) {
+            CreatePresenceSensorCommand cpsc = (CreatePresenceSensorCommand) command;
+            Node psNode = phat.deviceConfig.getDevicesAppState().getDevice(cpsc.getPresenceSensorId());
+            if (psNode != null) {
+                PHATPresenceSensor psControl = psNode.getControl(PHATPresenceSensor.class);
+                if (psControl != null) {
+                    PresenceStatePanel psp1 = new PresenceStatePanel();
+                    psControl.add(psp1);
+                    sensorMonitor.getContentPane().add(psp1);
+                    sensorMonitor.pack();
+                    sendMQTTmessage(psNode.toString());
+                }
+            }
+        }
+    }
+
+    //TODO: Separar init de mensaje en si
+    private void sendMQTTmessage(String msg) {
+        try {
+            MqttClient sampleClient = new MqttClient(mqttBroker, mqttClientId, mqttPersistence);
+            MqttConnectOptions connOpts = new MqttConnectOptions();
+            connOpts.setCleanSession(true);
+            System.out.println("Connecting to broker: "+mqttBroker);
+            sampleClient.connect(connOpts);
+            System.out.println("Connected");
+            System.out.println("Publishing message: "+msg);
+            MqttMessage message = new MqttMessage(msg.getBytes(StandardCharsets.UTF_8));
+            message.setQos(mqttQos);
+            sampleClient.publish(mqttTopic, message);
+            System.out.println("Message published");
+            sampleClient.disconnect();
+            System.out.println("Disconnected");
+        } catch(MqttException me) {
+            System.out.println("reason "+me.getReasonCode());
+            System.out.println("msg "+me.getMessage());
+            System.out.println("loc "+me.getLocalizedMessage());
+            System.out.println("cause "+me.getCause());
+            System.out.println("excep "+me);
+            me.printStackTrace();
+        }
     }
 }
